988c8e708fc02fea0fb1a22994ee86ed542279fc
Erik Figueiredo <falecom@erikfigueiredo.com.br>
2015-02-05 14:01:33 -0200

    Desabilitado o DebugKit

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -162,7 +162,7 @@ Plugin::load('Migrations');
 // Only try to load DebugKit in development mode
 // Debug Kit should not be installed on a production system
 if (Configure::read('debug')) {
-    Plugin::load('DebugKit', ['bootstrap' => true]);
+#    Plugin::load('DebugKit', ['bootstrap' => true]);
 }
 /**
  * Connect middleware/dispatcher filters.
